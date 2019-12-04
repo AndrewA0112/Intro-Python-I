a862f2d476c396c3f86c970f44a32997412561fc
AndrewA0112 <andrew.mkal@outlook.com>
2019-12-03 18:16:48 -0600

    Ran loop through argv, and fixed platform/version log on 03

--- src/03_modules.py
+++ src/03_modules.py
@@ -10,15 +10,16 @@ import sys
 
 # Print out the command line arguments in sys.argv, one per line:
 # YOUR CODE HERE
-print(sys.argv)
+for arg in sys.argv:
+    print(arg)
 
 # Print out the OS platform you're using:
 # YOUR CODE HERE
-print("Windows 10")
+print(sys.platform)
 
 # Print out the version of Python you're using:
 # YOUR CODE HERE
-print("3.8.0")
+print(sys.version)
 
 
 import os
